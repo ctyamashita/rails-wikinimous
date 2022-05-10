0eb31afe644e2747cd2ea97a4de7e0b528d32b8f
Celso Yamashita <ctyamashita@hotmail.com>
2022-05-10 18:23:36 +0900

    minor correction

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to task_path(@article)
+      redirect_to article_path(@article)
     else
       render 'new.html.erb'
     end
